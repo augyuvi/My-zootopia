bc2e3923a77d5f645593dc10df74998a47b907a5
Nithya Srinivasan <nithyasrinivasan@Mac.fritz.box>
2025-09-01 09:58:55 +0200

    Access data from json file

---
+++ animals_web_generator.py
@@ -0,0 +1,18 @@
+import json
+
+def load_data(file_path):
+    """Loads a JSON file"""
+    with open(file_path, "r") as handle:
+        return json.load(handle)
+animals_data = load_data('animals_data.json')
+for animal in animals_data:
+       if "name" in animal:
+        print(f"Name: {animal['name']}")
+       if "diet" in animal:
+        print(f"Diet: {animal['diet']}")
+       if "locations" in animal and len(animal["locations"]) > 0:
+        print(f"Location: {animal['locations'][0]}")
+       if "type" in animal:
+        print(f"Type: {animal['type']}")
+
+print()  # blank line between animals
